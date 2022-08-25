600566dab3b2370a36d3180d840b07ec3b95f609
zafiris argiros <zafiris.argiros69@outlook.com>
2022-08-25 13:44:27 +0300

    a

---
+++ MyDatabase/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDatabase
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        //public ApplicationDbContext() : base("Sindesmos")
+        //{
+           
+        //}
+
+
+    }
+}
